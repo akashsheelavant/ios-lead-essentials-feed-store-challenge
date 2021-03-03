ddf6c64d803c873290058ac27c9649d77fee9bb2
Akash Sheelavant <ASheelavant@walmart.com>
2021-03-03 17:26:40 +0530

    Delivers Found Values On Non Empty Cache.

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -36,10 +36,43 @@ public class CoreDataFeedStore: FeedStore {
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-
+		let context = self.context
+		context.perform {
+			do {
+				let cache = CoreDataCache(context: context)
+				cache.timeStamp = timestamp
+				cache.feed = NSSet(array: feed.map { local in
+					let feed = CoreDataFeedImage(context: context)
+					feed.id = local.id
+					feed.imageDescription = local.description
+					feed.location = local.location
+					feed.url = local.url
+					return feed
+				})
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+		}
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			do {
+				let request = NSFetchRequest<CoreDataCache>(entityName: CoreDataCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				if let cache = try context.fetch(request).first {
+					let feed = cache.feed?.compactMap { $0 as? CoreDataFeedImage }
+						.map { LocalFeedImage(id: $0.id!, description: $0.imageDescription, location: $0.location, url: $0.url!) }
+					completion(.found(feed: feed!, timestamp: cache.timeStamp!))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 }
