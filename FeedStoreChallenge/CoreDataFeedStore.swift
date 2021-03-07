bfc9c604091bb7a86819a30fb69d88d728415449
Akash Seelavant <akashsheelavant81@gmail.com>
2021-03-07 19:03:45 +0530

    Avoiding force unwrapping of objects.

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -70,10 +70,11 @@ public class CoreDataFeedStore: FeedStore {
 		let context = self.context
 		context.perform {
 			do {
-				if let cache = try self.fetchCache() {
-					let feed = cache.feed?.compactMap { $0 as? CoreDataFeedImage }
-						.map { LocalFeedImage(id: $0.id!, description: $0.imageDescription, location: $0.location, url: $0.url!) }
-					completion(.found(feed: feed!, timestamp: cache.timeStamp!))
+				if let cache = try self.fetchCache(),
+				   let feed = cache.feed {
+					let feed = feed.compactMap { $0 as? CoreDataFeedImage }
+						.map { LocalFeedImage(id: $0.id ?? UUID(), description: $0.imageDescription, location: $0.location, url: $0.url ?? URL(string: "")!) }
+					completion(.found(feed: feed, timestamp: cache.timeStamp ?? Date()))
 				} else {
 					completion(.empty)
 				}
