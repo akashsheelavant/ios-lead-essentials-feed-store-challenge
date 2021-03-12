3134ca8ac1842128702acb573c865dfb1edd60af
Akash Seelavant <akashsheelavant81@gmail.com>
2021-03-12 14:50:14 +0530

    Fixing indentation.

--- FeedStoreChallenge/CoreDataHelper.swift
+++ FeedStoreChallenge/CoreDataHelper.swift
@@ -16,18 +16,18 @@ extension NSPersistentContainer {
 	
 	static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
 		guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
-					throw LoadingError.modelNotFound
-				}
-
-				let description = NSPersistentStoreDescription(url: url)
-				let container = NSPersistentContainer(name: name, managedObjectModel: model)
-				container.persistentStoreDescriptions = [description]
-
-				var loadError: Swift.Error?
-				container.loadPersistentStores { loadError = $1 }
-				try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
-
-				return container
+			throw LoadingError.modelNotFound
+		}
+		
+		let description = NSPersistentStoreDescription(url: url)
+		let container = NSPersistentContainer(name: name, managedObjectModel: model)
+		container.persistentStoreDescriptions = [description]
+		
+		var loadError: Swift.Error?
+		container.loadPersistentStores { loadError = $1 }
+		try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+		
+		return container
 	}
 }
 
